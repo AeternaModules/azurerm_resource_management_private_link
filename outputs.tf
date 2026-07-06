b38220e64f19f9eef2e387fccd1a4600a8f5d038
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:42 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "resource_management_private_links" {
   description = "All resource_management_private_link resources"
   value       = azurerm_resource_management_private_link.resource_management_private_links
 }
+output "resource_management_private_links_location" {
+  description = "List of location values across all resource_management_private_links"
+  value       = [for k, v in azurerm_resource_management_private_link.resource_management_private_links : v.location]
+}
+output "resource_management_private_links_name" {
+  description = "List of name values across all resource_management_private_links"
+  value       = [for k, v in azurerm_resource_management_private_link.resource_management_private_links : v.name]
+}
+output "resource_management_private_links_resource_group_name" {
+  description = "List of resource_group_name values across all resource_management_private_links"
+  value       = [for k, v in azurerm_resource_management_private_link.resource_management_private_links : v.resource_group_name]
+}
 
