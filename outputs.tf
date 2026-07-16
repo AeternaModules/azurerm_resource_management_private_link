958d0a5d5d206b2d9d956b83ec758bcfeab68807
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "resource_management_private_links_id" {
   description = "Map of id values across all resource_management_private_links, keyed the same as var.resource_management_private_links"
-  value       = { for k, v in azurerm_resource_management_private_link.resource_management_private_links : k => v.id }
+  value       = { for k, v in azurerm_resource_management_private_link.resource_management_private_links : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "resource_management_private_links_location" {
   description = "Map of location values across all resource_management_private_links, keyed the same as var.resource_management_private_links"
-  value       = { for k, v in azurerm_resource_management_private_link.resource_management_private_links : k => v.location }
+  value       = { for k, v in azurerm_resource_management_private_link.resource_management_private_links : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "resource_management_private_links_name" {
   description = "Map of name values across all resource_management_private_links, keyed the same as var.resource_management_private_links"
-  value       = { for k, v in azurerm_resource_management_private_link.resource_management_private_links : k => v.name }
+  value       = { for k, v in azurerm_resource_management_private_link.resource_management_private_links : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "resource_management_private_links_resource_group_name" {
   description = "Map of resource_group_name values across all resource_management_private_links, keyed the same as var.resource_management_private_links"
-  value       = { for k, v in azurerm_resource_management_private_link.resource_management_private_links : k => v.resource_group_name }
+  value       = { for k, v in azurerm_resource_management_private_link.resource_management_private_links : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 
